854f08885dead39c8702ee529005d9080b0b35dd
Stephanie <stefniogu@gmail.com>
2023-04-26 16:09:08 +0100

    create a class Follow

---
+++ java/src/ie/tudublin/d22126511/Follow.java
@@ -0,0 +1,28 @@
+package ie.tudublin.d22126511;
+
+public class Follow {
+    private String word;
+    private int count;
+
+    public Follow (String word, int count){
+        this.word = word;
+        this.count = count;
+    }
+
+    public void setWord(String word){
+        this.word = word;
+    }
+
+    public String getWord(){
+       return this.word;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    public int getCount(){
+        return this.count;
+     }
+
+}
