bfeed640608634680a70f8f58f931b89f1974280
Stephanie <stefniogu@gmail.com>
2023-04-26 16:18:30 +0100

    load file method created

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,8 +6,6 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
-
 	public void settings() {
 		size(1000, 1000);
 		//fullScreen(SPAN);
@@ -15,15 +13,16 @@ public class DANI extends PApplet {
 
     String[] sonnet;
 
-    public String[] writeSonnet()
-    {
+    public String[] writeSonnet() {
         return null;
     }
 
 	public void setup() {
 		colorMode(HSB);
 
-       
+		for (String string : loadFile()) {
+			System.out.println(string);
+		}
 	}
 
 	public void keyPressed() {
@@ -32,13 +31,23 @@ public class DANI extends PApplet {
 
 	float off = 0;
 
-	public void draw() 
-    {
+	public void draw() {
 		background(0);
 		fill(255);
 		noStroke();
 		textSize(20);
         textAlign(CENTER, CENTER);
-        
+	}
+
+	public String[] loadFile(){
+		String[] lines = loadStrings("shakespere.txt"); // Load a text file into a String array
+		for (String line : lines) {
+			line.toLowerCase(); // Convert a string to lower case 
+			String words[] = split(line, ' '); // Split a string into an array of words
+			for (String word : words) {
+				word = word.replaceAll("[^\\w\\s]",""); // Remove punction characters
+			}
+		}
+		return lines;
 	}
 }
